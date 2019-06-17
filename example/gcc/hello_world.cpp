52d5b5335223db04eae2fe9bc7d320962e01ef97
Patrick Huhn <phuhn@ikf.uni-frankfurt.de>
2019-06-17 14:16:41 +0200

    add example of gcc

---
+++ example/gcc/hello_world.cpp
@@ -0,0 +1,17 @@
+/*
+
+compile the code with
+
+g++ hello_world.cpp -o hello_world -Wall
+ 
+*/
+
+// include a header file, needed for the cout command
+#include <iostream>
+
+int main()
+{
+  std::cout << "hello world" << std::endl;
+  return 0;
+}
+
